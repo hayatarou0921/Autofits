1bd1fe2e9e33b1e3cfd6b52391abf175233f1b3b
Hayatarou <Hayatarou0921@gmai.com>
2019-07-20 22:03:25 +0900

    Generate shoes function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :tops, only: [:index, :new, :create, :destroy]
   resources :trousers, only: [:index, :new, :create, :destroy]
+  resources :shoes, only: [:index, :new, :create, :destroy]
   devise_for :users
-  
+
   root to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
 end
