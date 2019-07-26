bf2d8d2e97c85ec65c6bc5379019a1890da9e8b3
Hayatarou <Hayatarou0921@gmai.com>
2019-07-26 09:39:33 +0900

    微調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,32 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require bootstrap
-// require_tree .
-
-
-$(document).on('turbolinks:load', function(){
-  $fileField = $('#file')
- 
-  // 選択された画像を取得し表示
-  $($fileField).on('change', $fileField, function(e) {
-    file = e.target.files[0]
-    reader = new FileReader(),
-    $preview = $("#img-field");
- 
-    reader.onload = (function(file) {
-      return function(e) {
-        $preview.empty();
-        $preview.append($('<img>').attr({
-          src: e.target.result,
-          class: "preview",
-          title: file.name
-        }));
-      };
-    })(file);
-    reader.readAsDataURL(file);
-  });
-});
+//=  require_tree .
